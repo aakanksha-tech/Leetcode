7eddbb9e64a1dd424865be7f849f37be96da7e1a
Aakanksha Bugad <akanksha.bugad@gmail.com>
2026-07-01 16:15:53 +0530

    Time: 1 ms (88.18%) | Memory: 44.1 MB (57.16%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -7,12 +7,21 @@ class Solution {
     }
 
     void backtrack(int index, int[] nums, List<Integer> temp) {
-        ans.add(new ArrayList<>(temp));
 
-        for (int i = index; i < nums.length; i++) {
-            temp.add(nums[i]);
-            backtrack(i + 1, nums, temp);
-            temp.remove(temp.size() - 1);
+        // Base Case
+        if (index == nums.length) {
+            ans.add(new ArrayList<>(temp));
+            return;
         }
+
+        // Include current element
+        temp.add(nums[index]);
+        backtrack(index + 1, nums, temp);
+
+        // Backtrack
+        temp.remove(temp.size() - 1);
+
+        // Exclude current element
+        backtrack(index + 1, nums, temp);
     }
 }
